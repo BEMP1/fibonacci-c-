688522c7bbf9af3ca632191807ea927bd2ad28ff
Bruno Moyano <108307019+BEMP1@users.noreply.github.com>
2022-10-25 08:42:58 -0300

    Add files via upload

---
+++ fibo c++.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int fibo(int num){
+	
+	if(num==0)
+		return num;
+	if(num==1)
+		return num;
+	
+	return fibo(num-1)+fibo(num-2);
+	
+}
+
+int main(int argc, char *argv[]) {
+	int num;
+	cout<<"ingrese sucesiones";
+	cin>>num;
+	cout<<fibo(num);
+	return 0;
+}
+
+
